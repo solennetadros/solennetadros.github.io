a5e4747b011899c3a1354e5b91a1909b2af67b1e
Solenne Tadros <tadrs749@newschool.edu>
2016-02-05 22:24:24 -0500

    i did awesome work

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var rectW = 100;
+var rectH = 60;
+
+function setup () {
+  createCanvas (1400, 800);
+}
+
+  function draw() {
+    background (0);
+    for (var rectX=0; rectX<width; rectX+=120) {
+      for (var rectY = 0; rectY<height; rectY+=80){
+        if ( (mouseX > rectX) && (mouseX < rectX +rectW) && 
+           (mouseY > rectY) && (mouseY< rectY + rectH )) { 
+      fill (random(255), random(255), random(255));
+    } else {
+      fill (255) ;
+    }
+    rect (rectX, rectY, rectW, rectH);
+
+    }
+  }
+}
